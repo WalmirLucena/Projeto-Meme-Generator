c3ff14a3268d79db5bcb44f5d48c252551cae8b1
WalmirLucena <walmirlucena3@gmail.com>
2021-07-19 11:25:40 -0400

    Adiciona os Arquivos

---
+++ script.js
@@ -0,0 +1,34 @@
+const memeText = document.getElementById('meme-text');
+const getInput = document.getElementById('text-input');
+const getImageContainer = document.getElementById('meme-image');
+const memeContainer = document.getElementById('meme-image-container');
+
+getInput.addEventListener('keyup', () => {
+  memeText.innerHTML = getInput.value;
+});
+
+function uploadImage() {
+  const getImage = document.getElementById('meme-insert');
+  getImage.addEventListener('change', (event) => {
+    getImageContainer.src = URL.createObjectURL(event.target.files[0]);
+  });
+}
+
+uploadImage();
+const getButtons = document.getElementById('buttons');
+
+getButtons.addEventListener('click', (event) => {
+  const buttonId = event.target.id;
+  if (buttonId === 'fire') {
+    memeContainer.style.border = '3px dashed red';
+  } else if (buttonId === 'water') {
+    memeContainer.style.border = '5px double blue';
+  } else if (buttonId === 'earth') {
+    memeContainer.style.border = '6px groove green';
+  }
+});
+
+const getImageSaved = document.getElementById('image-saved');
+getImageSaved.addEventListener('click', (event) => {
+  getImageContainer.src = event.target.src;
+});
